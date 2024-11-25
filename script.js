96faa86b5b8708d66924a31d657816e44ef90a2a
Pranav Panchal <pranavpanchal303@gmail.com>
2024-11-25 02:00:28 -0500

    walls added

--- script.js
+++ script.js
@@ -18,21 +18,32 @@ function setup () {
 function draw () {
   background(51)
   for (i = 0; i < grid.length; i++) {
-    // console.log(i)
     grid[i].show()
-    // console.log(grid)
   }
 }
 
 function Cell (i, j) {
   this.i = i
   this.j = j
+  this.walls = [true, true, true, true]
 
   this.show = function () {
     let x = this.i * w
     let y = this.j * w
     stroke(255)
-    noFill()
-    rect(x, y, w, w)
+    if (this.walls[0]) {
+      line(x, y, x + w, y)
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w)
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w)
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y)
+    }
+    // noFill()
+    // rect(x, y, w, w)
   }
 }
